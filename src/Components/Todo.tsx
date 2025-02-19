e88137f32e94d760ae00cc4c0728127a3f7a6493
John Devaney <johndevaney1@hotmail.com>
2025-02-19 17:51:02 +0000

    Now able to fetch data and post data with tanstack

--- src/Components/Todo.tsx
+++ src/Components/Todo.tsx
@@ -1,23 +1,18 @@
+import { useIsFetching } from "@tanstack/react-query";
 import { useTodosIds } from "../services/queries";
 
-export default function Todo(){
-    const todosIdsQuery = useTodosIds();
-    
-    if(todosIdsQuery.isPending)
-    {
-        return <span>loading...</span>
-    }
-    if(todosIdsQuery.isError)
-    {
-        return <span>There is an error</span>
-    }
+export default function Todo() {
+  const todosIdsQuery = useTodosIds();
+  const isFetching = useIsFetching();
 
-    return(
-        <>
-            <p>Query function status: {todosIdsQuery.fetchStatus}</p>
-            {todosIdsQuery.data.map((id) => (
-                <p key={id}>{id}</p>
-            ))}
-        </>
-    );
+  return (
+    <>
+      <p>Query function status: {todosIdsQuery.fetchStatus}</p>
+      <p>Query data status: {todosIdsQuery.status}</p>
+      <p>Global isFetching: {isFetching}</p>
+      {todosIdsQuery.data?.map((id) => (
+        <p key={id}>{id}</p>
+      ))}
+    </>
+  );
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -5,5 +5,10 @@ const BASE_URL = "http://localhost:8080"
 const axiosInstance = axios.create({baseURL:BASE_URL})
 
 export const getTodosIds = async() => {
-    return (await axiosInstance.get<Todo[]>('todos')).data.map((todo) => todo.id);
+    return (await axiosInstance.get<Todo[]>('todos'))
+    .data.map((todo) => todo.id);
+}
+
+export const getTodo = async(id: number) => {
+    return(await axiosInstance.get<Todo>(`todo/${id}`)).data;
 }

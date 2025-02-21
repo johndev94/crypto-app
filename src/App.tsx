8c1408e1652383ef7c1e09c44154fe6fd02707ed
John Devaney <johndevaney1@hotmail.com>
2025-02-21 18:23:51 +0000

    Pagination working

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Todos from "./Components/Todo";
+import Projects from "./Components/Projects";
 
 function App() {
   return (
     <>
-      <Todos />
+      <Projects />
+      {/* <Todos /> */}
     </>
   );
 }
